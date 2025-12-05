cc3d948b39daee2aaba03781cc183fc3179e3b45
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-12-05 12:48:32 +0900

    feat: 리뷰 soft delete

--- src/main/java/com/popjub/reviewservice/application/service/ReviewService.java
+++ src/main/java/com/popjub/reviewservice/application/service/ReviewService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.popjub.reviewservice.application.dto.command.CreateReviewCommand;
 import com.popjub.reviewservice.application.dto.result.CreateReviewResult;
+import com.popjub.reviewservice.application.dto.result.DeleteReviewResult;
 import com.popjub.reviewservice.application.dto.result.SearchReviewResult;
 import com.popjub.reviewservice.domain.entity.Review;
 import com.popjub.reviewservice.domain.repository.ReviewRepository;
@@ -52,5 +53,16 @@ public class ReviewService {
 	public Page<SearchReviewResult> getReviewsByStoreId(UUID storeId, Pageable pageable) {
 		Page<Review> reviews = reviewRepository.findAllByStoreId(storeId, pageable);
 		return reviews.map(SearchReviewResult::from);
+	}
+
+	public DeleteReviewResult deleteReview(Long userId, UUID reviewId) {
+
+		Review review = reviewRepository
+			.findByReviewIdAndUserId(reviewId, userId)
+			.orElseThrow(() -> new RuntimeException("리뷰가 없거나 삭제 권한이 없습니다."));
+		review.delete(userId);
+		reviewRepository.delete(review);
+
+		return DeleteReviewResult.from(review.getReviewId());
 	}
 }
